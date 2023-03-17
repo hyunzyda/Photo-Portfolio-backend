a4716d87e8d08c83f0965336d95425273954709b
Hyunji Lim <guswlwptlzk@gmail.com>
2023-03-17 18:51:38 +0900

    HJ 0317

--- src/main/java/com/kosmo/project/dto/User.java
+++ src/main/java/com/kosmo/project/dto/User.java
@@ -1,5 +1,11 @@
 package com.kosmo.project.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
 public class User {
 
     private int id;
@@ -9,44 +15,4 @@ public class User {
 
     public User() {
     }
-
-    public User(int id, String userid, String userps, String username) {
-        this.id = id;
-        this.userid = userid;
-        this.userps = userps;
-        this.username = username;
-    }
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUserid() {
-		return userid;
-	}
-
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-
-	public String getUserps() {
-		return userps;
-	}
-
-	public void setUserps(String userps) {
-		this.userps = userps;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
 }
