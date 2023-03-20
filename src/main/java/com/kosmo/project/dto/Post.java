300560f40c0f9c58025e4338899e0089369af538
Hyunji Lim <guswlwptlzk@gmail.com>
2023-03-20 15:35:44 +0900

    0320hj

--- src/main/java/com/kosmo/project/dto/Post.java
+++ src/main/java/com/kosmo/project/dto/Post.java
@@ -1,6 +1,6 @@
 package com.kosmo.project.dto;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,14 +8,14 @@ import lombok.Setter;
 
 @Getter @Setter
 @AllArgsConstructor
-public class PostDTO {
+public class Post {
 
 	private int post_id;
 	private String content;
 	private String image_url;
 	private int user_id;
-	private LocalDate created_at;
-	private LocalDate modified_at;
+	private LocalDateTime created_at;
+	private LocalDateTime modified_at;
 	
-	public PostDTO() {}
+	public Post() {}
 }
