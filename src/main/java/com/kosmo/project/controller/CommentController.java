59b5b5c89465a1e63f674d25e9dfc198631e18f9
Junyoung Kim <wnsdud2021@naver.com>
2023-03-23 15:38:22 +0900

    Comment 완성

--- src/main/java/com/kosmo/project/controller/CommentController.java
+++ src/main/java/com/kosmo/project/controller/CommentController.java
@@ -1,10 +1,14 @@
 package com.kosmo.project.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,9 +26,10 @@ public class CommentController {
 	@Autowired
 	private CommentDAO commentDao;
 	
+	// 게시물별 댓글 조회
 	@GetMapping("/{id}")
-	public ResponseEntity<Comment> getComment(@PathVariable(value="id") int id){
-		Comment comment = commentDao.getCommentById(id);
+	public ResponseEntity<List<Comment>> getComment(@PathVariable(value="id") int id){
+		List<Comment> comment = commentDao.getCommentById(id);
 		if(comment == null) {
 			return ResponseEntity.notFound().build();
 		}
@@ -32,14 +37,25 @@ public class CommentController {
 	}
 	
 	// 댓글 추가
-	@PostMapping("/add")
-	public ResponseEntity<Comment> addComment(@Valid @RequestBody Comment comment){
-		boolean result = commentDao.addComment(comment);
+	@PostMapping("/{id}")
+	public ResponseEntity<Comment> addComment(@PathVariable(value="id") int post_id ,@Valid @RequestBody Comment comment){
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();
+		boolean result = commentDao.addComment(email,post_id,comment);
 		if(result) {
 			return ResponseEntity.status(HttpStatus.CREATED).build();
 		} else {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-		
+		}	
 	}
+	
+	// 댓글 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable(value="id") int id) {
+        boolean result = commentDao.deleteCommentById(id);
+        if (result) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }	
 }
